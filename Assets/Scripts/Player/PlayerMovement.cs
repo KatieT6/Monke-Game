17d2b19cf7a98ad734d1c1dbd3b795ae957e7185
MAIPA01 <marcelipatryk@gmail.com>
2024-10-30 16:41:31 +0100

    Mario Jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,7 +18,10 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jump Settings: ")]
     public float jumpHeight = 10f;
-    public float jumpTime = 1f;
+    public float jumpStopSpeed = 3f;
+    private bool jumped = false;
+    private float jumpTime = 0f;
+    private float jumpVelocity = 0f;
 
     [Header("Move Settings: ")]
     public float moveSpeed = 10f;
@@ -33,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        #region INIT_INPUT
         if (input == null)
         {
             input = GetComponent<PlayerInput>();
@@ -40,23 +44,38 @@ public class PlayerMovement : MonoBehaviour
             attackAction = input.actions["Attack"];
             jumpAction = input.actions["Jump"];
         }
+        #endregion
 
+        #region ATTACK
         if (attackAction.triggered)
         {
             // Attack
         }
+        #endregion
 
-        Vector2 toMove = moveAction.ReadValue<Vector2>();
-        toMove.x *= moveSpeed;
-
+        #region JUMP
         if (jumpAction.triggered && IsOnGround)
         {
             // Jump
-            float jumpVelocity = (jumpHeight + 0.5f * Physics2D.gravity.y * jumpTime * jumpTime) / jumpTime;
-            toMove += Vector2.up * jumpVelocity;
+            jumpTime = Mathf.Sqrt((-2f * jumpHeight) / Physics2D.gravity.y);
+            jumpVelocity = -Physics2D.gravity.y * jumpTime;
+            rb.linearVelocityY = jumpVelocity;
+            jumped = true;
+        }
+        else if (!jumpAction.IsPressed() && !IsOnGround && rb.linearVelocityY > 0f && jumped)
+        {
+            rb.linearVelocityY = Mathf.Lerp(rb.linearVelocityY, 0f, Time.deltaTime * jumpStopSpeed);
         }
 
-        rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, toMove.x, Time.deltaTime * velocityChangeSpeed);
-        rb.linearVelocityY += toMove.y;
+        if (jumped && rb.linearVelocityY <= 0f)
+        {
+            jumped = false;
+        }
+        #endregion
+
+        #region MOVE_HORIZONTAL
+        float move = moveAction.ReadValue<Vector2>().x * moveSpeed;
+        rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, move, Time.deltaTime * velocityChangeSpeed);
+        #endregion
     }
 }
